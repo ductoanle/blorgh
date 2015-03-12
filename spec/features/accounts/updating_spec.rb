7e8354bacb3ca4a3a310f5c7ae658041aced286a
Ethan Le <ductoanle@gmail.com>
2015-03-12 09:58:11 +0800

    add updating specs

---
+++ spec/features/accounts/updating_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'multitenacy/testing_support/factories/account_factory'
+require 'multitenacy/testing_support/authentication_helpers'
+
+feature 'Accounts' do
+  include Multitenacy::TestingSupport::AuthenticationHelpers
+  let(:account){ FactoryGirl.create(:account) }
+  let(:root_url) { "http://#{account.subdomain}.example.com"}
+
+  context 'as the account owner' do
+    before do
+      sign_in_as(user: account.owner, account: account)
+    end
+
+    scenario 'updating an account' do
+      visit root_url
+      click_link 'Edit Account'
+      fill_in 'Name', with: 'A new name'
+      click_button 'Update Account'
+      expect(page).to have_content('Account updated successfully.')
+      expect(account.reload.name).to eq 'A new name'
+    end
+  end
+end
